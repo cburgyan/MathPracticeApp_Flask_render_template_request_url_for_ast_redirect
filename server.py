7c1cb2c2bcf5b14364c3401887703cd3916b1ed9
cburgyan <cburgyan@gmail.com>
2022-07-22 20:47:58 -0500

    pass operands, symbol, answer, and color into practice.html

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
-
+import random
 
 app = Flask(__name__)
-
+DIGITS = 3
 
 @app.route('/')
 def main_page():
@@ -11,8 +11,18 @@ def main_page():
 @app.route('/practice/<operation>')
 def practice_page(operation):
     if operation == 'addition':
-        pass
-    return render_template('practice.html', operation=operation)
+        coloring = 'primary'
+        oper1 = random.randint(10 ** (DIGITS - 1), 10 ** DIGITS - 1)
+        oper2 = random.randint(10 ** (DIGITS - 1), 10 ** DIGITS - 1)
+        answer = oper1 + oper2
+        symbol = "fa-solid fa-plus"
+    elif operation == 'subtraction':
+        coloring = 'warning'
+    elif operation == 'multiplication':
+        coloring = 'info'
+    elif operation == 'division':
+        coloring = 'dark'
+    return render_template('practice.html', operation=operation, coloring=coloring, operand1=oper1, operand2=oper2, answer=answer, symbol=symbol)
 
 
 
